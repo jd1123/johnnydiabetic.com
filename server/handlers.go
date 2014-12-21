bd32a598770b7bb9f17afc0254252fbc58cbd531
johnnydiabetic <johnnydiabetic@gmail.com>
2014-12-21 12:50:35 -0500

    Added RobotsHandler and updated the content

--- server/handlers.go
+++ server/handlers.go
@@ -1,7 +1,10 @@
 package server
 
 import (
+	"io/ioutil"
+	"log"
 	"net/http"
+	"os"
 
 	"github.com/jd1123/johnnydiabetic.com/helpers"
 )
@@ -17,4 +20,16 @@ func AboutHandler(w http.ResponseWriter, r *http.Request) {
 
 func StaticHandler(w http.ResponseWriter, r *http.Request) {
 	http.ServeFile(w, r, r.URL.Path[1:])
+}
+
+func RobotsHandler(w http.ResponseWriter, r *http.Request) {
+	f, err := os.Open("static/robots.txt")
+	if err != nil {
+		log.Printf("IO Error")
+	} else {
+		r, err := ioutil.ReadAll(f)
+		if err != nil {
+		}
+		w.Write(r)
+	}
 }
--- server/routes.go
+++ server/routes.go
@@ -5,6 +5,7 @@ import "net/http"
 type HandlerFunction func(w http.ResponseWriter, r *http.Request)
 
 var routes = map[string]HandlerFunction{
-	"/":       IndexHandler,
-	"/about/": AboutHandler,
+	"/":           IndexHandler,
+	"/about/":     AboutHandler,
+	"/robots.txt": RobotsHandler,
 }
