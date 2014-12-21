54a37e431fc18076df556e6b994b67f48b342bcc
johnnydiabetic <johnnydiabetic@gmail.com>
2014-12-21 10:26:54 -0500

    Minor html updates

--- helpers/templates.go
+++ helpers/templates.go
@@ -1,7 +1,6 @@
 package helpers
 
 import (
-	"fmt"
 	"net/http"
 	"path"
 	"text/template"
@@ -15,6 +14,5 @@ func Template(name string) string {
 
 func RunTemplate(w http.ResponseWriter, templateName, baseTemplateName string, data interface{}) {
 	tmpl := template.Must(template.New(templateName).ParseFiles(baseTemplateName, templateName))
-	fmt.Println("Executing a template")
 	tmpl.ExecuteTemplate(w, "base", data)
 }
--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 
 	"github.com/jd1123/johnnydiabetic.com/server"
@@ -11,5 +12,5 @@ func main() {
 	fmt.Println("Listening on port 8081")
 	server.RegisterHandlers()
 	//http.Handle("/static", http.FileServer(http.Dir("static")))
-	http.ListenAndServe(":8081", nil)
+	log.Fatal(http.ListenAndServe(":8081", nil))
 }
